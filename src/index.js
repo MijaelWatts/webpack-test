e407574d37132a29618d71bac70f78cc4235805e
Lopez, Victor <na>
2019-05-28 19:18:56 -0500

    Asset Management section completed.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import jsPng from './js_icon.png';
+import Data from './data.xml';
 
 function component() {
+    const myIcon  = new Image();
     const element = document.createElement('div');
     
+    myIcon.src = jsPng;
+
     element.innerHTML = _.join(['Hello', 'Webpack'], '');
+    element.classList.add('hello');
+    element.appendChild(myIcon);
+
+    console.log(Data);
 
     return element;
 }
